76ef722df8206d29b7590e96edf4f44135722d84
Thiago Silva <48453887+ThiagoOliveiira@users.noreply.github.com>
2021-02-24 21:33:54 -0300

    Update test_pytestes.py

--- test_pytestes.py
+++ test_pytestes.py
@@ -2,6 +2,6 @@ import pytest
 from app import soma
 
 
-@pytest.mark.parametrize('a,b,expected', [(5, 4, 9)])
+@pytest.mark.parametrize('a,b,expected', [(5, 4, 9)], [-2, -4, 6])
 def test_soma(a,b, expected):
     assert soma(a,b) == expected
